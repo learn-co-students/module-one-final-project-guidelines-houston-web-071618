b2e5124426251c5d9f742e1a6fecac3ffd18244e
Alejandro Aguilar Jr <alejandro.aguilar.jr@gmail.com>
2018-07-31 15:53:06 -0500

    added musix_match gem and api key

--- bin/run.rb
+++ bin/run.rb
@@ -1,5 +1,15 @@
 require_relative '../config/environment'
+require "pry"
+require "musix_match"
 
-
+MusixMatch::API::Base.api_key = 'd8f358f97300518be509afbb7bcc131a'
 
 puts "HELLO WORLD"
+response = MusixMatch.search_track(:q_artist => 'Arctic Monkeys')
+if response.status_code == 200
+  response.each do |track|
+    puts "#{track.track_id}: #{track.track_name} (#{track.artist_name})"
+  end
+end
+# binding.pry
+# false
