af393ba92f1479c989869933b34c35a5c72b4098
Alejandro Aguilar Jr <alejandro.aguilar.jr@gmail.com>
2018-08-01 16:56:58 -0500

    seed data

--- bin/run.rb
+++ bin/run.rb
@@ -2,7 +2,6 @@ require_relative '../config/environment'
 require "pry"
 require "musix_match"
 
-binding.pry
 MusixMatch::API::Base.api_key = "d8f358f97300518be509afbb7bcc131a"
 
 # puts "HELLO WORLD"
@@ -14,18 +13,22 @@ MusixMatch::API::Base.api_key = "d8f358f97300518be509afbb7bcc131a"
 #     # puts "Lyrics id: #{track.lyrics_id}"
 #   end
 # end
+count = 0
 
-
-response = MusixMatch.search_track(:q_artist => 'The Beatles', :page_size => 50, :f_has_lyrics => true, :s_track_rating => 'desc', f_lyrics_language: 'en' )
+response = MusixMatch.get_track_chart(:page_size => 50, :f_has_lyrics => true)
 if response.status_code == 200
   response.each do |track|
-
-      lyric_response = MusixMatch.get_lyrics(track.track_id)
-      if lyric_response.status_code == 200 && lyrics = lyric_response.lyrics
-        # Song.new(title: track.track_name , usix_match_track_id:track.track_id , lyrics:lyrics.lyrics_body , artist_id:1 , category_id:1)
+    lyric_response = MusixMatch.get_lyrics(track.track_id)
+    if lyric_response.status_code == 200 && lyrics = lyric_response.lyrics
+      if lyrics.lyrics_body != ""
+        blah=lyrics.lyrics_body.split("...")
+        count+=1
+        puts "*******************************************************************************************************"
         puts "#{track.track_id}: #{track.track_name} (#{track.artist_name})"
-        puts "#{lyrics.lyrics_body[0..50]}"
+        puts "#{blah.first}"
+        puts "*******************************************************************************************************"
       end
+    end
   end
 end
 
