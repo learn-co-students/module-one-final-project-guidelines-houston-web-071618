ec23c7ff40acb428b86c3bba647190fc4ff4e5fc
github username <github email address>
2018-08-01 17:29:30 -0500

    cli

--- bin/run.rb
+++ bin/run.rb
@@ -2,50 +2,21 @@ require_relative '../config/environment'
 require "pry"
 require "musix_match"
 
-#binding.pry
 MusixMatch::API::Base.api_key = "d8f358f97300518be509afbb7bcc131a"
 
-# puts "HELLO WORLD"
-# response = MusixMatch.search_track(:q_artist => 'The Beatles', :page_size => 50, :f_has_lyrics => true, :s_track_rating => 'desc', f_lyrics_language: 'en' )
+# response = MusixMatch.search_track(:q_artist => 'The Beatles', :page_size => 50, :f_has_lyrics => true, :s_track_rating => 'desc', f_lyrics_language: 'en', f_track_release_group_first_release_date_min: 19620101, f_track_release_group_first_release_date_max: 19700701 )
 # if response.status_code == 200
 #   response.each do |track|
-#
-#     puts "#{track.track_id}: #{track.track_name} (#{track.artist_name})"
-#     # puts "Lyrics id: #{track.lyrics_id}"
+#     #binding.pry
+#     lyric_response = MusixMatch.get_lyrics(track.track_id)
+#     if lyric_response.status_code == 200 && lyrics = lyric_response.lyrics
+#       # Song.new(title: track.track_name , usix_match_track_id:track.track_id , lyrics:lyrics.lyrics_body , artist_id:1 , category_id:1)
+#       puts "#{track.track_id}: #{track.track_name} (#{track.artist_name})"
+#       #puts "#{lyrics.lyrics_body[0..50]}"
+#     end
 #   end
 # end
 
-
-response = MusixMatch.search_track(:q_artist => 'The Beatles', :page_size => 50, :f_has_lyrics => true, :s_track_rating => 'desc', f_lyrics_language: 'en', f_track_release_group_first_release_date_min: 19620101, f_track_release_group_first_release_date_max: 19700701 )
-if response.status_code == 200
-  response.each do |track|
-      #binding.pry
-      lyric_response = MusixMatch.get_lyrics(track.track_id)
-      if lyric_response.status_code == 200 && lyrics = lyric_response.lyrics
-        # Song.new(title: track.track_name , usix_match_track_id:track.track_id , lyrics:lyrics.lyrics_body , artist_id:1 , category_id:1)
-        puts "#{track.track_id}: #{track.track_name} (#{track.artist_name})"
-        #puts "#{lyrics.lyrics_body[0..50]}"
-      end
-  end
-end
-
-# response = MusixMatch.get_lyrics('3799553')
-# if response.status_code == 200 && lyrics = response.lyrics
-#   puts lyrics.lyrics_body
-#   #binding.pry
-# end
-#
-# response = MusixMatch.get_lyrics('3799553')
-# if response.status_code == 200 && lyrics = response.lyrics
-#   puts lyrics.lyrics_body[0..100]
-#   #binding.pry
-# end
-#
-# response = MusixMatch.get_track_chart(:page_size => 50)
-# if response.status_code == 200
-#   response.each do |track|
-#     puts "#{track.track_id}: #{track.track_name} (#{track.artist_name})"
-#   end
-# end
- #binding.pry
-# false
+new_cli = CommandLineInterface.new
+new_cli.run
+# input = new_cli.gets_user_input
---
+++ lib/cli.rb
@@ -0,0 +1,117 @@
+class CommandLineInterface
+
+  @@score = 0
+  @@hint = 0
+
+  def greet
+    puts 'Welcome to Lyrics Trivia, get ready to test your music knowledge!'
+    puts "Let's play."
+    puts "Enter a topic from the following options:"
+    puts "The Beatles\nJustin Beiber\nBeyonce/Destiny's Child\nToday's Hits\n2000's Pop\n90's Pop\n"
+  end
+
+  def gets_user_input
+    gets.chomp
+  end
+
+  def find_category(input)
+    if Category.find_by(title: input) == nil
+      puts "Sorry, that option is not available yet. Please try again from the options given."
+      input = gets_user_input
+      find_category(input)
+    else
+      Category.find_by(title: input)
+    end
+  end
+
+  def get_lyrics(categ)
+    selection = Song.select do |song|
+      categ.id == song.category_id
+    end
+    puts selection.sample
+    selection.sample
+  end
+
+  def show_lyrics(game_song)
+    puts "\nHere's some lyrics: \n#{game_song.lyrics[0..50]}\n\n"
+  end
+
+  def prompt_answer_or_hint
+    puts "To guess the song select 1, or to get more lyrics select 2.\n"
+    while user_input = gets.chomp
+     case user_input
+     when "1"
+       return 1
+     when "2"
+       return 2
+     else
+       puts "Please select either 1 or 2"
+       prompt_answer_or_hint
+     end
+    end
+  end
+
+  def show_all_lyrics(song)
+    @@hint = 5
+    puts "\n#{song.lyrics}\n"
+  end
+
+  def prompt_answer
+    puts "\nPlease type your answer:\n"
+  end
+
+  def compare_answer(answer, song)
+    parsed_song = song.title.split(' (')
+    parsed_song = parsed_song.first
+    if parsed_song.downcase == answer.downcase
+      puts "\nCongrats! You got it right!\n"
+      @@score = @@score + 10 - @@hint
+      puts "Your score is now #{@@score}"
+    else
+      puts "Sorry, wrong answer.\n Answer is: \n#{parsed_song}"
+      puts "Your score is now #{@@score}"
+    end
+  end
+
+  def prompt_play_again
+    puts "\n Wanna play again? Type y or n:\n"
+  end
+
+  def repeat_or_exit
+    while play_again = gets.chomp
+      if play_again == "y"
+        puts "Let's do it!"
+        run
+      elsif play_again == "n"
+        puts "Your final score is: #{@@score}.\n\nBYE!"
+        return true
+      else
+        puts "Please select either y or n"
+        repeat_or_exit
+      end
+    end
+  end
+
+  def run
+    exitted = false
+    greet
+    input = gets_user_input
+    categ = find_category(input)
+    game_song = get_lyrics(categ)
+    show_lyrics(game_song)
+    input2 = prompt_answer_or_hint
+    if input2 == 2
+      show_all_lyrics(game_song)
+    end
+    prompt_answer
+    answer = gets_user_input
+    compare_answer(answer, game_song)
+    prompt_play_again
+    exitted = repeat_or_exit
+    if exitted == true
+      return
+    end
+  end
+
+
+end
