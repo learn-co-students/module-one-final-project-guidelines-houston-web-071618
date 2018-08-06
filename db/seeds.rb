ddfca1e5ade20863ba1c6a25ac214f7c151e3767
github username <github email address>
2018-08-06 18:06:42 -0500

    added artist seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,7 @@
 require_relative '../config/environment.rb'
 require 'json'
 require 'rest-client'
+require 'csv'
 
 Artist.delete_all
 Track.delete_all
@@ -24,5 +25,26 @@ def seed_tracks(artist_array)
 	end
 end
 
-seed_tracks(artist_array)
-seed_artists
+def artists_from_csv
+	CSV.foreach('./db/enriched_artist_data.csv', headers: true) do |row|
+		# x = encode_text(row["bio_intro"])
+		# binding.pry
+		Artist.create({
+			name: row["corrected_band_name"],
+			mbid: row["mbid"],
+			listeners: row["listeners"],
+	    cost: row["average_price"],
+	    wikipedia_bio_intro: row["bio_intro"],
+	    actual_known_cost?: row["actual_known_cost?"],
+	    image_url: row["image_url"]
+		})
+	end
+end
+
+def encode_text(text)
+	text.class == String ? text.encode!('UTF-8', 'binary', invalid: :replace, undef: :replace, replace: '') : text
+end
+
+artists_from_csv
+# seed_tracks(artist_array)
+# seed_artists
